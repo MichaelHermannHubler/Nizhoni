4d0eaba9135bb2b0c34b3a689d81319baf30b96f
Michael Hermann-Hubler <39239606+MichaelHermannHubler@users.noreply.github.com>
2022-03-07 21:57:11 +0100

    Window Events added

--- Nizhoni/src/Nizhoni/Application.cpp
+++ Nizhoni/src/Nizhoni/Application.cpp
@@ -7,12 +7,23 @@
 #include <GLFW/glfw3.h>
 
 namespace Nizhoni {
+#define BIND_EVENT_FN(x) std::bind(&x, this, std::placeholders::_1)
+
 	Application::Application() {
 		m_Window = std::unique_ptr<Window>(Window::Create());
+		m_Window->SetEventCallback(BIND_EVENT_FN(Application::OnEvent));
 	}
 
 	Application::~Application() {
 
+	}
+
+	void Application::OnEvent(Event& e)
+	{
+		EventDispatcher dispatcher(e);
+		dispatcher.Dispatch<WindowCloseEvent>(BIND_EVENT_FN(Application::OnWindowClose));
+
+		NI_CORE_TRACE("{0}", e);
 	}
 
 	void Application::Run() {
@@ -21,5 +32,11 @@ namespace Nizhoni {
 			glClear(GL_COLOR_BUFFER_BIT);
 			m_Window->OnUpdate();
 		}
+	}
+
+	bool Application::OnWindowClose(WindowCloseEvent& e)
+	{
+		m_Running = false;
+		return true;
 	}
 }
--- Nizhoni/src/Nizhoni/Application.h
+++ Nizhoni/src/Nizhoni/Application.h
@@ -1,6 +1,9 @@
 #pragma once
 
 #include "Nizhoni/Core.h"
+#include "Events/Event.h"
+#include "Nizhoni/Events/ApplicationEvent.h"
+
 #include "Window.h"
 
 namespace Nizhoni {
@@ -10,11 +13,17 @@ namespace Nizhoni {
 		std::unique_ptr<Window> m_Window;
 		bool m_Running = true;
 
+
 	public:
 		Application();
 		virtual ~Application();
+
 		void Run();
 
+		void OnEvent(Event& e);
+
+	private:
+		bool OnWindowClose(WindowCloseEvent& e);
 	};
 
 	//To be defined in Client
