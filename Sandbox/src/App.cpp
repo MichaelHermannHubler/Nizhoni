c78ac25390375345161232e6f7d9dc3328b1928e
Michael Hermann-Hubler <39239606+MichaelHermannHubler@users.noreply.github.com>
2022-03-13 23:26:50 +0100

    Moved Platform location

--- Sandbox/src/App.cpp
+++ Sandbox/src/App.cpp
@@ -3,13 +3,7 @@
 class App : public Nizhoni::Application {
 public:
 	App() {
-		NI_SET_CORE_LOGLEVEL(NI_LOG_LEVEL_TRACE);
-
-		NI_CORE_CRITICAL("Critical");
-		NI_CORE_ERROR("Error");
-		NI_CORE_WARN("Warn");
-		NI_CORE_INFO("Info");
-		NI_CORE_TRACE("Trace");
+		NI_SET_CORE_LOGLEVEL(NI_LOG_LEVEL_WARN);
 	}
 
 	~App() {
