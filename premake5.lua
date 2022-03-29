2787e0781a0e51c0ecbc03ca006a3a319e331d87
Michael Hermann-Hubler <39239606+MichaelHermannHubler@users.noreply.github.com>
2022-03-29 11:28:20 +0200

    bugfix

--- premake5.lua
+++ premake5.lua
@@ -65,7 +65,7 @@ project "Nizhoni"
 		}
 
 		postbuildcommands {
-			("{MKDIR ../bin/" .. outputdir .. "/Sandbox}; {COPY} %{cfg.buildtarget.relpath} ../bin/" .. outputdir .. "/Sandbox")
+			("{MKDIR} ../bin/" .. outputdir .. "/Sandbox; {COPY} %{cfg.buildtarget.relpath} ../bin/" .. outputdir .. "/Sandbox")
 		}
 
 	filter "configurations:Debug"
